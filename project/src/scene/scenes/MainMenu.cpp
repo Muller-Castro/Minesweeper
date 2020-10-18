af28668be7a83453b06d58b6636e6ae012982eba
Muller-Castro <37383316+Muller-Castro@users.noreply.github.com>
2020-10-17 22:11:28 -0300

    Update MainMenu.cpp

--- project/src/scene/scenes/MainMenu.cpp
+++ project/src/scene/scenes/MainMenu.cpp
@@ -1,5 +1,8 @@
 #include "scene/scenes/MainMenu.h"
 
+#include "Input.h"
+#include "scene/SceneManager.h"
+
 using namespace Minesweeper;
 
 MainMenu::MainMenu()
@@ -14,7 +17,7 @@ MainMenu::~MainMenu()
 
 void MainMenu::process_inputs()
 {
-    //
+    if(Input::is_just_pressed<Input::Key>(sf::Keyboard::Enter)) SceneManager::change_scene_to(SceneManager::Scenes::SPLASH_SCREEN);
 }
 
 void MainMenu::update(float delta)
