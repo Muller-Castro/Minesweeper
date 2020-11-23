b95581f0b3af063354ea387cf830bdebd3be031d
Muller-Castro <37383316+Muller-Castro@users.noreply.github.com>
2020-11-23 19:55:29 -0300

    Create CreditsButton

---
+++ project/include/components/buttons/CreditsButton.h
@@ -0,0 +1,52 @@
+/****************************************************************************************/
+/* CreditsButton.h                                                                      */
+/****************************************************************************************/
+/* Copyright (c) 2020 Muller Castro.                                                    */
+/*                                                                                      */
+/* Permission is hereby granted, free of charge, to any person obtaining                */
+/* a copy of this software and associated documentation files (the "Software"),         */
+/* to deal in the Software without restriction, including without limitation            */
+/* the rights to use, copy, modify, merge, publish, distribute, sublicense,             */
+/* and/or sell copies of the Software, and to permit persons to whom the Software       */
+/* is furnished to do so, subject to the following conditions:                          */
+/*                                                                                      */
+/* The above copyright notice and this permission notice shall be included in all       */
+/* copies or substantial portions of the Software.                                      */
+/*                                                                                      */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,  */
+/* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A        */
+/* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT   */
+/* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF */
+/* CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE */
+/* OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                        */
+/****************************************************************************************/
+
+#ifndef CREDITS_BUTTON_H
+#define CREDITS_BUTTON_H
+
+#include <functional>
+
+#include "components/Button.h"
+
+namespace Minesweeper {
+
+    class MainMenu;
+
+    class CreditsButton final : public Button
+    {
+    public:
+        CreditsButton(MainMenu& mm_ref_, const sf::Vector2f& position_, const sf::Vector2f& scale_, const std::shared_ptr<sf::Texture>& hovered, const std::shared_ptr<sf::Texture>& non_hovered, const std::shared_ptr<sf::Texture>& down);
+        ~CreditsButton() override {}
+
+    protected:
+        void on_button_up()      override;
+        void on_button_down()    override;
+        void on_button_pressed() override;
+
+    private:
+        std::reference_wrapper<MainMenu> mm_ref;
+    };
+
+}
+
+#endif // CREDITS_BUTTON_H
