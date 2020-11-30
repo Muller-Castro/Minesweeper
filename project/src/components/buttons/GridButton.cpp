e528a3e9ec9186070b4603873a2fa26cc3fc8dba
Muller-Castro <37383316+Muller-Castro@users.noreply.github.com>
2020-11-29 21:02:24 -0300

    Create GridButton

---
+++ project/src/components/buttons/GridButton.cpp
@@ -0,0 +1,61 @@
+/****************************************************************************************/
+/* GridButton.cpp                                                                       */
+/****************************************************************************************/
+/* Copyright (c) 2020 Muller Castro.                                                    */
+/*                                                                                      */
+/* Permission is hereby granted, free of charge, to any person obtaining                */
+/* a copy of this software and associated documentation files (the "Software"),         */
+/* to deal in the Software without restriction, including without limitation            */
+/* the rights to use, copy, modify, merge, publish, distribute, sublicense,             */
+/* and/or sell copies of the Software, and to permit persons to whom the Software       */
+/* is furnished to do so, subject to the following conditions:                          */
+/*                                                                                      */
+/* The above copyright notice and this permission notice shall be included in all       */
+/* copies or substantial portions of the Software.                                      */
+/*                                                                                      */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,  */
+/* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A        */
+/* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT   */
+/* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF */
+/* CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE */
+/* OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                        */
+/****************************************************************************************/
+
+#include "components/buttons/GridButton.h"
+
+using namespace Minesweeper;
+
+GridButton::GridButton(const sf::Vector2f& position_, const sf::Vector2f& scale_, const std::shared_ptr<sf::Texture>& hovered, const std::shared_ptr<sf::Texture>& non_hovered, const std::shared_ptr<sf::Texture>& down, const std::shared_ptr<sf::SoundBuffer>& hovered_sfx, const std::shared_ptr<sf::SoundBuffer>& pressed_sfx) :
+    Button(position_, scale_, hovered, non_hovered, down, hovered_sfx, pressed_sfx)
+{
+    //
+}
+
+void GridButton::update(float delta)
+{
+    Button::update(delta);
+
+    if(state != States::PRESSED) {
+
+        bounding_box.top    += 1.f;
+        bounding_box.left   += 1.f;
+        bounding_box.width  -= 1.f;
+        bounding_box.height -= 1.f;
+
+    }
+}
+
+void GridButton::on_button_up()
+{
+    //
+}
+
+void GridButton::on_button_down()
+{
+    //
+}
+
+void GridButton::on_button_pressed()
+{
+    //
+}
