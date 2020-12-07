9d0238bdd61f820ebf22fd5abf1918d2ab7b251f
Muller-Castro <37383316+Muller-Castro@users.noreply.github.com>
2020-12-06 21:36:34 -0300

    Create raw game textures

---
+++ project/src/assets/EmptyCell.cpp
@@ -0,0 +1,42 @@
+/****************************************************************************************/
+/* EmptyCell.cpp                                                                        */
+/****************************************************************************************/
+/* Copyright (c) 2020 Muller Castro.                                                    */
+/*                                                                                      */
+/* Permission is hereby granted, free of charge, to any person obtaining                */
+/* a copy of this software and associated documentation files (the "Software"),         */
+/* to deal in the Software without restriction, including without limitation            */
+/* the rights to use, copy, modify, merge, publish, distribute, sublicense,             */
+/* and/or sell copies of the Software, and to permit persons to whom the Software       */
+/* is furnished to do so, subject to the following conditions:                          */
+/*                                                                                      */
+/* The above copyright notice and this permission notice shall be included in all       */
+/* copies or substantial portions of the Software.                                      */
+/*                                                                                      */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,  */
+/* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A        */
+/* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT   */
+/* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF */
+/* CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE */
+/* OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                        */
+/****************************************************************************************/
+
+#include "assets/EmptyCell.h"
+
+#include <vector>
+
+std::pair<std::string, std::string> Minesweeper::get_raw_empty_cell()
+{
+	std::pair<std::string, std::string> result = std::make_pair("EmptyCell", "");
+
+	std::vector<short> raw_data {
+		
+		0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a,
+		0x1a, 0x0a,
+		0x00, 0x00, 0x00, 0x0d, 0x49, 0x48, 0x44, 0x52, 0x00, 0x00, 0x00, 0x14, 0x00, 0x00, 0x00, 0x14, 0x08, 0x02, 0x00, 0x00, 0x00, 0x02, 0xeb, 0x8a, 0x5a, 0x00, 0x00, 0x00, 0x01, 0x73, 0x52, 0x47, 0x42, 0x01, 0xd9, 0xc9, 0x2c, 0x7f, 0x00, 0x00, 0x00, 0x20, 0x63, 0x48, 0x52, 0x4d, 0x00, 0x00, 0x7a, 0x26, 0x00, 0x00, 0x80, 0x84, 0x00, 0x00, 0xfa, 0x00, 0x00, 0x00, 0x80, 0xe8, 0x00, 0x00, 0x75, 0x30, 0x00, 0x00, 0xea, 0x60, 0x00, 0x00, 0x3a, 0x98, 0x00, 0x00, 0x17, 0x70, 0x9c, 0xba, 0x51, 0x3c, 0x00, 0x00, 0x00, 0x09, 0x70, 0x48, 0x59, 0x73, 0x00, 0x00, 0x0b, 0x13, 0x00, 0x00, 0x0b, 0x13, 0x01, 0x00, 0x9a, 0x9c, 0x18, 0x00, 0x00, 0x00, 0x1e, 0x49, 0x44, 0x41, 0x54, 0x38, 0xcb, 0x63, 0xf0, 0xa7, 0x00, 0x30, 0x00, 0xf1, 0x1e, 0xb2, 0xc0, 0xa8, 0xe6, 0x51, 0xcd, 0xa3, 0x9a, 0x47, 0x35, 0x23, 0x6b, 0x26, 0x1b, 0x00, 0x00, 0xfb, 0x10, 0x10, 0x5a, 0xfc, 0xa2, 0xdd, 0xdb, 0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4e, 0x44, 0xae, 0x42, 0x60, 0x82
+	};
+
+	for(short x : raw_data) result.second += static_cast<char>(x);
+
+	return result;
+}
