7769567a5acce0d5fafb34e98d4db777eabe5816
Muller-Castro <37383316+Muller-Castro@users.noreply.github.com>
2021-01-03 23:41:11 -0300

    Create ClearButton

---
+++ project/src/components/buttons/ClearButton.cpp
@@ -0,0 +1,50 @@
+/****************************************************************************************/
+/* ClearButton.cpp                                                                      */
+/****************************************************************************************/
+/* Copyright (c) 2020 Muller Castro.                                                    */
+/*                                                                                      */
+/* Permission is hereby granted, free of charge, to any person obtaining                */
+/* a copy of this software and associated documentation files (the "Software"),         */
+/* to deal in the Software without restriction, including without limitation            */
+/* the rights to use, copy, modify, merge, publish, distribute, sublicense,             */
+/* and/or sell copies of the Software, and to permit persons to whom the Software       */
+/* is furnished to do so, subject to the following conditions:                          */
+/*                                                                                      */
+/* The above copyright notice and this permission notice shall be included in all       */
+/* copies or substantial portions of the Software.                                      */
+/*                                                                                      */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,  */
+/* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A        */
+/* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT   */
+/* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF */
+/* CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE */
+/* OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                        */
+/****************************************************************************************/
+
+#include "components/buttons/ClearButton.h"
+
+#include "components/TextEdit.h"
+
+using namespace Minesweeper;
+
+ClearButton::ClearButton(TextEdit& te, Enabled enabled_, const sf::Vector2f& position_, const sf::Vector2f& scale_, const std::shared_ptr<sf::Texture>& hovered, const std::shared_ptr<sf::Texture>& non_hovered, const std::shared_ptr<sf::Texture>& down, const std::shared_ptr<sf::SoundBuffer>& hovered_sfx, const std::shared_ptr<sf::SoundBuffer>& pressed_sfx) :
+    Button(enabled_, position_, scale_, hovered, non_hovered, down, hovered_sfx, pressed_sfx),
+    text_edit_ref(te)
+{
+    //
+}
+
+void ClearButton::on_button_up()
+{
+    //
+}
+
+void ClearButton::on_button_down()
+{
+    //
+}
+
+void ClearButton::on_button_pressed()
+{
+    text_edit_ref.get().clear();
+}
